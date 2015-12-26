1710cf5784cc3c7979b3d1b9a46a8759481fd218
Daniel Carvalho <mail@danielcarvalho.pt>
2015-12-26 02:01:46 +0000

    Adds site files

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery-migrate-1.2.1.min.js
+//= require bootstrap.min.js
+//= require bootstrap-hover-dropdown.min.js
+//= require back-to-top.js
+//= require jquery.placeholder.js
+//= require smooth-scroll.min.js
+//= require jquery.flexslider-min.js
+//= require animatedModal.min.js
+//= require photoswipe.min.js
+//= require photoswipe-ui-default.min.js
+//= require jquery.waterwheelCarousel.js
+//= require main.js
+
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+    def home
+    end
+
+    def cultural
+    end
+
+    def social
+    end
+
+    def educativo
+    end
+
+    def sobre
+    end
+
+    def contactos
+    end
+end
